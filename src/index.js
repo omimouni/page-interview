69747ad3816043009e45cd7ad301492bf54c30a6
Oumar Mimouni <omimouni@student.1337.ma>
2020-11-14 01:23:19 +0100

    final but performance still low

--- src/index.js
+++ src/index.js
@@ -4,29 +4,33 @@ import "swiper/swiper-bundle.css";
 import Swiper from "swiper";
 import anime from "animejs/lib/anime.es.js";
 
-
-const loading = document.getElementsByClassName('loading')[0];
+const loading = document.getElementsByClassName("loading")[0];
 
 anime({
   targets: ".loading",
   bottom: 0,
   delay: 800,
-  complete: e => loading.innerHTML = "Loading ..."
+  complete: (e) => (loading.innerHTML = "Loading ..."),
 });
 
-window.addEventListener('load', e => {
+window.addEventListener("load", (e) => {
   anime({
     targets: loading.parentNode,
     opacity: 0,
     delay: 100,
-    complete: e => loading.parentNode.remove()
-  })
+    complete: (e) => loading.parentNode.remove(),
+  });
 });
 
 const swiper = new Swiper(".swiper-container", {
-  slidesPerView: 3,
+  slidesPerView: 2,
   centeredSlidesBounds: true,
   spaceBetween: 20,
+  breakpoints: {
+    1024: {
+      slidesPerView: 3,
+    },
+  },
 });
 
 const shopItems = document.getElementsByClassName("shop-item");
@@ -50,15 +54,17 @@ for (let i = 0; i < shopItems.length; i++) {
   });
 }
 
-const button = document.getElementById("policy-button");
-button.addEventListener("click", (e) =>
-  anime({
-    targets: button.parentNode,
-    opacity: 0,
-    delay: 100,
-    complete: e => button.parentElement.remove()
-  })
-);
+const buttons = document.getElementsByClassName("policy-button");
+for (let i = 0; i < buttons.length; i++) {
+  buttons[i].addEventListener("click", (e) =>
+    anime({
+      targets: buttons[i].parentNode,
+      opacity: 0,
+      delay: 100,
+      complete: (e) => button[i].parentElement.remove(),
+    })
+  );
+}
 
 anime({
   targets: ".up-anime",
@@ -71,33 +77,30 @@ const closeMenu = document.getElementById("close");
 
 openMenu.addEventListener("click", (e) => {
   e.preventDefault();
-  anime.set('.menuboard', {
+  anime.set(".menuboard", {
     translateX: 0,
   });
 
-  anime.set('.link', {
-    bottom: 30
+  anime.set(".link", {
+    bottom: 30,
   });
   anime({
-    targets: '.link',
-    bottom: 0
+    targets: ".link",
+    bottom: 0,
   });
-
 });
 
 closeMenu.addEventListener("click", (e) => {
   e.preventDefault();
-  anime.set('.menuboard', {
-    translateX: "100%"
+  anime.set(".menuboard", {
+    translateX: "100%",
   });
-  
 });
 
 const img = document.getElementById("menuCover");
 const links = document.getElementsByClassName("link");
 
 for (let i = 0; i < links.length; i++) {
-
   links[i].addEventListener("mouseover", (e) => {
     img.setAttribute("src", links[i].getAttribute("data-img"));
   });
